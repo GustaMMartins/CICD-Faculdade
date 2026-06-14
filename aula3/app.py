3ffe3dcd47f308cfeda02b84a99a64826845c981
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-14 14:01:42 -0300

    ci: criado aula3/app.py e tbm aula3/requirements.txt

---
+++ aula3/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bem-vindo à Aula 3"
+
+@app.route("/sobre")
+def sobre():
+    return "Exemplo de API Flask para CI/CD"
+
+@app.route("/status")
+def status():
+    return {
+        "status": "online",
+        "aplicacao": "Aula 3"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
