82692679cebb15d4031a145098d51938824a5941
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-13 22:25:28 -0300

    Feat: Criando rota autores

---
+++ aula2/flask-worflow/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Pagina Inicial"
+
+    @app.route("/sobre")
+def sobre():
+    return "Pagina Sobre"
+
+
+@app.route("/livros")
+def livros():
+    return "Lista de Livros"
+
+
+@app.route("/autores")
+def autores():
+    return "Lista de autores"
+
