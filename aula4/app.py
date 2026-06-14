6fd2e2a1c3c8e6fa236aa222b037487b2ecf3e7d
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-14 14:54:51 -0300

    ci: test criado e realizado com sucesso

---
+++ aula4/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bem-vindo à Aula 4"
+
+@app.route("/sobre")
+def sobre():
+    return "Exemplo de API Flask para o CI/CD"
+
+@app.route("/status")
+def status():
+    return {
+        "status": "online",
+        "aplicacao": "Aula 4"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
