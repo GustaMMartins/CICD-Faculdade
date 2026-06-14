fe0601fd5dd20aaf4aff18c9a51209a23b9139d5
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-13 22:16:22 -0300

    Feat: criado rota livros

---
+++ aula2/flask-worflow/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Pagina Inicial"
+
+    @app.route("/sobre")
+def sobre():
+    return "Pagina Sobre"
+
+
+@app.route("/livros")
+def livros():
+    return "Lista de Livros"
+
