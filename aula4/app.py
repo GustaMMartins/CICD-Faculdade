6115c639b6452140e68fe4c9f357834fab01247a
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-14 15:33:49 -0300

    clex: Foi criado dois teste, um de soma e outro de divisao, foi realizado os dois teste, e os dois foi  sucesso com o pytest

--- aula4/app.py
+++ aula4/app.py
@@ -1,21 +1,5 @@
-from flask import Flask
+def soma(a, b):
+    return a + b
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bem-vindo à Aula 4"
-
-@app.route("/sobre")
-def sobre():
-    return "Exemplo de API Flask para o CI/CD"
-
-@app.route("/status")
-def status():
-    return {
-        "status": "online",
-        "aplicacao": "Aula 4"
-    }
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def divisao(a, b):
+    return a / b
